5283f3422175475981960479806662a44ceb9d22
NawalOmari <nawal.omari7@gmail.com>
2025-06-24 20:10:06 -0400

    Update Student.cs

--- StudentEFCodeFirstApp/Student.cs
+++ StudentEFCodeFirstApp/Student.cs
@@ -8,5 +8,9 @@ namespace StudentEFCodeFirstApp
 {
     internal class Student
     {
+        public int Id { get; set; }           // Primary key (auto-inferred by EF)
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
     }
 }
