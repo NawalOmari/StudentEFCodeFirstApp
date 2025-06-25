049cea595f2989aaaff3aa3e53827bf2cce0f0fc
NawalOmari <nawal.omari7@gmail.com>
2025-06-24 20:10:55 -0400

    Update StudentContext.cs

--- StudentEFCodeFirstApp/StudentContext.cs
+++ StudentEFCodeFirstApp/StudentContext.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace StudentEFCodeFirstApp
 {
-    internal class StudentContext
+    public class StudentContext : DbContext
     {
+        public StudentContext() : base("StudentDBConnection") { }
+
+        public DbSet<Student> Students { get; set; }
     }
 }
